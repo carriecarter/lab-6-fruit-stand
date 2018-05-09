bc2d6d7651ac16c8551c44cd7e54346e41562dbd
carriecarter <carrie@carrie-carter.com>
2018-05-08 18:15:09 -0700

    app.js in progress

--- js/app.js
+++ js/app.js
@@ -1,2 +1,23 @@
 'use-strict';
+/* globals fruits */
 
+function allFruits() {
+    var fruits = document.getElementById("fruit-list");
+    for(var i = 0; i < fruits.length; i++){
+        var li = fruits[i].createLi();
+        list.appendChild(li);
+        
+    }
+}
+
+// function fruitStand() {
+//     var firstFruit = fruits[0];
+//     var fruitList = document.getElementById("fruit-list");
+    
+//     fruitList.appendChild(createLi());
+    
+//     //fruitList.appendChild(fruitStand.createLi());
+//     console.log('cat' , fruits[0].createLi());
+// }
+
+allFruits();
--- js/fruit.js
+++ js/fruit.js
@@ -1,53 +1,56 @@
+/* export fruits */
 
-var fruits = [ 
+var fruits = [
     {
-      apple = {
-          img: 'apple.png',
-          origin: 'usa',
-          color: 'green',
-          season: 'fall',
-    createApple: function() {
+        name: 'apple',
+        img: 'apple.png',
+        origin: 'usa',
+        color: 'green',
+        season: 'fall',
+        
+        createLi: function() {
         var li = document.createElement('li');
         li.textContent = 'origin: ' + this.origin + ' peak season: ' + this.season;
         return li;
-    }
-     },
-      cherry = {
-          img: 'cherry.png',
-          origin: 'usa',
-          color: 'green',
-          season: 'summer',
-    createCherry: function() {
+        }
+    },
+    {
+        name: 'cherry',
+        img: 'cherry.png',
+        origin: 'usa',
+        color: 'green',
+        season: 'summer',
+        
+        createLi: function() {
         var li = document.createElement('li');
         li.textContent = 'origin: ' + this.origin + ' peak season ' + this.season;
         return li;
-    }
-     },
-      grapes = {
-          img: 'grapes.png',
-          origin: 'france',
-          color: 'purple',
-          season: 'spring',
-    createGrapes: function() {
+        }
+    },
+    {
+        name: 'grapes',
+        img: 'grapes.png',
+        origin: 'france',
+        color: 'purple',
+        season: 'spring',
+        
+        createLi: function() {
         var li = document.createElement('li');
         li.textContent = 'origin: ' + this.origin + ' peak season ' + this.season;
         return li; 
-    }
-      },
-      banana = {
-          img: 'banana.png',
-          origin: 'brazil',
-          color: 'yellow',
-          season: 'summer',
-    createBanana: function() {
+        }
+    },
+    {
+        name: 'banana',
+        img: 'banana.png',
+        origin: 'brazil',
+        color: 'yellow',
+        season: 'summer',
+        
+        createLi: function() {
         var li = document.createElement('li');
         li.textContent = 'origin: ' + this.origin + ' peak season ' + this.season;
         return li;
-    }
-      },  
-    }
-
-
-
-
+        }
+    },  
 ]
